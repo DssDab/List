d650224060f68235a390f282ff1e9aea09b0c080
a <dume1425@gmail.com>
2025-09-11 09:40:13 +0900

    25_09_11
    Swap 함수 만들기

---
+++ 25일차.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250806
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 10;
+            int y = 20;
+            Swap(ref x, ref y);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+        static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
