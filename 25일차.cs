1c9e2ec0ad2b56b4dfc37653d008b51501511e85
a <dume1425@gmail.com>
2025-09-11 14:38:30 +0900

    문제풀이
    최대최소값 찾기랑 문자 개수 찾기

--- 25일차.cs
+++ 25일차.cs
@@ -16,12 +16,89 @@ namespace _20250806
 
             Console.WriteLine(x);
             Console.WriteLine(y);
+
+            Console.WriteLine(solution(580010));
+
+            int[] arr = new int[] { 13, 15, 1, 50, 99 };
+            MinMax(arr);
+
+            CountingStar(Console.ReadLine());
         }
         static void Swap(ref int x, ref int y)
         {
             int temp = x;
             x = y;
             y = temp;
+        }
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        //static int solution(int n)
+        //{
+        //    int answer = 0;
+        //    answer = n % 6 == 0 ? (n / 6) : (n*6 / GCD(n,6))/6;
+        //    return answer;
+        //}
+        //static int solution(int slice, int n)
+        //{
+        //    int answer = 0;
+        //    answer = n % slice == 0 ? n/ slice : n / slice + 1 ;
+        //    return answer;
+        //}
+        static int solution(int price)
+        {
+            int answer = 0;
+
+            if (price >= 100000 && price < 300000)
+                answer = (int)(price * 0.95);
+            else if (price >= 300000 && price < 500000)
+                answer = (int)(price * 0.9);
+            else if (price >= 500000)
+                answer = (int)(price * 0.8);
+            else
+                answer = price;
+            return answer;
+        }
+        static void MinMax(int[] arr)
+        {
+            int min, max;
+
+            min = arr[0];
+
+            max = arr[arr.Length - 1];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                    min = arr[i];
+                if (max < arr[i])
+                    max = arr[i];
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+        }
+        static int[] CountingStar(string s)
+        {
+            int[] str = new int[26];
+
+            for(int i=0; i < s.Length; i++)
+            {
+                str[s[i] - 'a']++;
+            }
+
+            foreach(int i in str)
+            {
+                Console.Write(i+" ");
+            }
+
+            return str;
         }
     }
 }
